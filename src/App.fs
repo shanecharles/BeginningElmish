6ecfe78f433fbdd1052cf2b6e36c9604a6c895fc
Shane Charles <shanecharles@burningicesolutions.com>
2018-07-22 22:09:44 -0500

    Add first messages and some buttons.

--- src/App.fs
+++ src/App.fs
@@ -3,20 +3,39 @@ module Client
 open Elmish
 open Elmish.React
 
-open Fable.Helpers.React
+open Fable.Helpers.React.Props
 
-type Model = string
+module R = Fable.Helpers.React
 
-type Msg = unit
+type Model = string option
+
+type Msg = 
+     | Clicked
+     | Reset
 
 let init () : Model * Cmd<Msg> =
-    "Initialized", Cmd.none
+    None, Cmd.none
 
 let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
-    model, Cmd.none
+    match msg with
+    | Clicked -> Some "You pressed the button!", Cmd.none
+    | Reset   -> None, Cmd.none
+
+let getOrElse otherValue value =
+    match value with 
+    | Some v -> v
+    | _      -> otherValue
 
 let view (model : Model) (dispatch : Msg -> unit) =
-    div [] [ str model ]
+    R.div [] 
+      [ R.button 
+          [ OnClick (fun _ -> dispatch Clicked) ] 
+          [ R.str "Click Me" ]
+        R.button
+          [ OnClick (fun _ -> dispatch Reset) ]
+          [ R.str "Reset" ]
+        R.div [] [ R.str (getOrElse "" model) ] 
+      ]
 
 #if DEBUG
 open Elmish.Debug
